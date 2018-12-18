f0023b5ae360035d8f79f0edd2aa5fbfaaac3bba
sanketpayghan <sanket.payghan8@gmail.com>
2018-12-18 17:45:30 +0530

    changed tests

---
+++ src/test/java/com/in28minutes/springboot/tutorial/basics/pojo/GreetingsTest.java
@@ -0,0 +1,28 @@
+package com.in28minutes.springboot.tutorial.basics.pojo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class GreetingsTest {
+
+    @Mock
+    private Greetings greetings;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void getMessage() {
+        when(greetings.getMessage()).thenReturn("Hello Sanket");
+        Assert.assertEquals("Hello", greetings.getMessage());
+    }
+}
